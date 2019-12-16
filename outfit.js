dbeafc19d512ec872c95970fc4d0bb9ddfa5895d
Cristina Pena <cristinalpena@gmail.com>
2019-12-16 13:17:56 -0700

    Add main.js and properties to outfit class

--- outfit.js
+++ outfit.js
@@ -1,6 +1,9 @@
 class Outfit {
-  constructor(){
-
+  constructor(id, title, garments, background){
+    this.id = 1;
+    this.title = 'Bear';
+    this.garments = [];
+    this.background = "Flowers";
   }
   addGarment(){
 
