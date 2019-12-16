bb48346be8acf9025476896fe11a30ae5a09fd60
Cristina Pena <cristinalpena@gmail.com>
2019-12-16 13:33:40 -0700

    Change values to parameters instead of hard-coding

--- outfit.js
+++ outfit.js
@@ -1,9 +1,9 @@
 class Outfit {
   constructor(id, title, garments, background){
-    this.id = 1;
-    this.title = 'Bear';
-    this.garments = [];
-    this.background = "Flowers";
+    this.id = id;
+    this.title = title;
+    this.garments = garments;
+    this.background = background;
   }
   addGarment(){
 
