4ca3bea68828985de8ac96233317dd41ccda582b
Harry Borrelli <hborrelli@outlook.com>
2019-12-16 23:04:17 -0700

    Psuedo code to refine approach to js

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 var outfits = [];
 var id = null;
 
-function newOutfit() {
+// Create new outfit on load
+newOutfit();
 
+function newOutfit() {
+  // Generate new unique id & set to global variable above
+  // now we can modify current outfit in global scope by variable above
+
+  // Push this outfit to 'outfits' global variable ^
+  outfits.push(new Outfit(id));
 }
+
+
+// add event listener to hats section
+
+
+// When clicking on a button add active class to button for visual display
+// Add garment item to outfit by targeting the id in global variable
+
+// If garment is already active, remove garment from object garment list.
+
+
+
+/* Notes */
+// Random ID is stored as ID property in new object
+// Random ID is also set to saved outfit buttons ID on right column
