59f408a2a9be33baa4bc30ddf34db0c746a3ed03
Harry Borrelli <hborrelli@outlook.com>
2019-12-17 11:16:17 -0700

    Pseudo code

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var hatSection = document.querySelector('.hats-list');
 
 hatSection.addEventListener('click', addGarment);
 
+// Create new outfit on load.
 newOutfit();
 
 function newOutfit() {
@@ -28,11 +29,15 @@ function addGarment(event) {
   // When clicking on a button add active class to button for visual display
   // Add garment item to outfit by targeting the id in global variable
 
-  // If garment is already active, remove garment from object garment list.
+  // If active state already exists, remove active class and item from Garments list.
+
 }
 
 
+// Toggle functions for managing garments in each category.
 
 /* Notes */
 // Random ID is stored as ID property in new object
 // Random ID is also set to saved outfit buttons ID on right column
+
+// How to ensure only one item from each category is added?
