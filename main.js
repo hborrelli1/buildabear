982739cf279d721466e3e3b0ed0152bfa0ee14b4
Harry Borrelli <hborrelli@outlook.com>
2019-12-18 11:42:12 -0700

    Add functionality to only allow one item per category.

--- main.js
+++ main.js
@@ -7,10 +7,6 @@ var backgroundsSection = document.querySelector('.backgrounds-list');
 var hatBtns = document.querySelectorAll('.hats-list .button-style');
 
 column1.addEventListener('click', addGarment);
-// hatSection.addEventListener('click', addGarment);
-// clothesSection.addEventListener('click', addGarment);
-// accessoriesSection.addEventListener('click', addGarment);
-// backgroundsSection.addEventListener('click', addGarment);
 
 // Create new outfit on load.
 var id = Date.now();
@@ -23,26 +19,11 @@ var newOutfit = new Outfit(id);
 // Add element id to index 0 of garments array.
 
 function addGarment(event) {
-  if (event.target.classList.contains('hat')) {
-    toggleActiveClass();
-    toggleGarment(0);
-  }
-
-  if (event.target.classList.contains('clothes')) {
-    toggleActiveClass();
-    toggleGarment(1);
-  }
-
-  if (event.target.classList.contains('accessories')) {
-    toggleActiveClass();
-    toggleGarment(2);
-  }
-
-  /*
   toggleGarments('hat', 0);
   toggleGarments('clothes', 1);
   toggleGarments('accessories', 2);
-  */
+  toggleGarments('backgrounds', 3);
+
   // If active state already exists, remove active class and item from Garments list.
 }
 
@@ -53,19 +34,6 @@ function toggleGarments(category, index) {
     toggleGarment(index);
   }
 }
-
-
-
-
-// function addGarment(event) {
-//   if (event.target.classList.contains('button-style')) {
-//     toggleActiveClass();
-//     toggleGarment();
-//   }
-//   // Add garment item to outfit by targeting the id in global variable
-//
-//   // If active state already exists, remove active class and item from Garments list.
-// }
 
 function toggleActiveClass() {
   if (event.target.classList.contains('active')){
@@ -78,11 +46,6 @@ function toggleActiveClass() {
 function toggleGarment(i) {
   newOutfit.garments.splice(i, 1, event.target.id);
 
-  // if(newOutfit.garments.includes(event.target.id)) {
-  //   outfits[currentOutfit].removeGarment(event.target.id);
-  // } else {
-  //   outfits[currentOutfit].addGarment(event.target.id);
-  // }
 }
 
 
